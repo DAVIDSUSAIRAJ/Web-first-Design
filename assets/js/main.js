16327c5c3f2e4d8663b7866d7c4b1f20f204db7c
David <davidsusairaj1996@gmail.com>
2021-12-14 02:55:58 +0530

    fixed navbar and scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 const navToggler = document.querySelector(".nav--menu__icon");
 
-
 navToggler.addEventListener("click", () => {
   const togglerMenu = document.querySelector(".nav--menu");
 
@@ -12,3 +11,20 @@ navToggler.addEventListener("click", () => {
     icon.classList.toggle("hidden");
   });
 });
+
+// NAVBAR FIXED WITH SCROLL BAR
+
+function navbarFixed(params) {
+  const headerDom = document.querySelector(".header");
+  const nav_ofset_top = headerDom.clientHeight + 35;
+  window.addEventListener("scroll", () => {
+    let scroll = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scroll >= nav_ofset_top) {
+      headerDom.classList.add("nav-fixed");
+    } else {
+      headerDom.classList.remove("nav-fixed");
+    }
+  });
+}
+navbarFixed();
