5866174a22f6a5deff2f74d276e778e271d6f773
David <davidsusairaj1996@gmail.com>
2021-12-15 03:45:54 +0530

    dynamic menu style

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,4 +27,30 @@ function navbarFixed(params) {
     }
   });
 }
-navbarFixed();
+// navbarFixed();
+
+function setMenuActive(params) {
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll(".nav--menu__list>a");
+
+  window.addEventListener("scroll", () => {
+    let current = "";
+    sections.forEach((section) => {
+      const sectionsTop = section.offsetTop;
+      const sectionsHeight = section.clientHeight;
+      if (pageYOffset >= sectionsTop - sectionsHeight / 8) {
+        current = section.getAttribute("id");
+      }
+    });
+    // end sections foreach;
+    // console.log(current);
+    // start navLinks foreach
+    navLinks.forEach((link) => {
+      link.classList.remove("active");
+      if (current == link.getAttribute("href").split("#")[1]) {
+        link.classList.add("active");
+      }
+    });
+  });
+}
+setMenuActive();
